771785ddda76957d8e311e1bfb388f170f69b22d
Tom Burrows <tomburrows13@icloud.com>
2020-09-30 23:01:15 +0100

    Change alternate control key to be valid control

--- prototypes/custom-input.lua
+++ prototypes/custom-input.lua
@@ -3,6 +3,6 @@ data:extend({
     type = "custom-input",
     name = "switch-spidertron-weapons",
     key_sequence = "CONTROL + TAB",
-    alternate_key_sequence = "\\",
+    alternate_key_sequence = "NONUSBACKSLASH",
   }
 })
