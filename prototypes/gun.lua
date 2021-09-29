d135c7951cefe9fd2974ebc4d8672d5c07647960
Tom Burrows <tomburrows13@icloud.com>
2021-09-29 15:52:53 +0100

    Overwrite MK2&3 rocket launcher names for consistency

--- prototypes/gun.lua
+++ prototypes/gun.lua
@@ -71,3 +71,19 @@ cannonmk3.name = "sws-cannon-mk3"
 cannonmk3.attack_parameters.range = 50
 
 data:extend{cannon, cannonmk2, cannonmk3}
+
+-- Rename spidertron-extended's MK2&3 rocket launcher's to be consistent with SWS's gun names
+local mk2_name = "spidertronmk2-rocket-launcher-"
+local mk3_name = "spidertronmk3-rocket-launcher-"
+for i = 1, 4 do
+  local guns = data.raw["gun"]
+  local mk2 = guns[mk2_name .. i]
+  if mk2 then
+    mk2.localised_name = {"item-name.spidertron-rocket-launcher-mk2"}
+  end
+  local mk3 = guns[mk3_name .. i]
+  if mk3 then
+    mk3.localised_name = {"item-name.spidertron-rocket-launcher-mk3"}
+  end
+
+end
