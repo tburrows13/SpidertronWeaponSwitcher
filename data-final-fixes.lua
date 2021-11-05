886c2987cdf620c9772ec140711792aeae0bbc16
Tom Burrows <tomburrows13@icloud.com>
2021-11-05 16:46:59 +0000

    1.2.8; Increase selection_priority of all vehicles

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -5,7 +5,8 @@ local insert = table.insert
 -- Add an extra layer to every prototype with collision mask of player-layer or rail-layer except spider-leg
 -- Replace spider-leg's masks with just spider_leg_layer
 
-local spider_leg_layers = {}  -- water-tile is temporary workaround for https://forums.factorio.com/viewtopic.php?f=7&t=100132
+local spider_leg_layers = collision_mask_util.get_default_mask("spider-leg")
+local spider_leg_alt_layers = {}
 
 if mods["space-exploration"] then
   insert(spider_leg_layers, "object-layer")
@@ -13,9 +14,10 @@ end
 
 log(serpent.block(collision_mask_util.get_default_mask("spider-leg")))
 log(serpent.block(collision_mask_util.get_mask(data.raw["spider-leg"]["spidertron-leg-1"])))
-for _, layer in pairs(collision_mask_util.get_default_mask("spider-leg")) do
+
+for _, layer in pairs(spider_leg_layers) do
   local spider_leg_layer = collision_mask_util.get_first_unused_layer()
-  insert(spider_leg_layers, spider_leg_layer)
+  insert(spider_leg_alt_layers, spider_leg_layer)
 
   local prototypes = collision_mask_util.collect_prototypes_with_layer(layer)
 
@@ -37,5 +39,24 @@ for _, layer in pairs(collision_mask_util.get_default_mask("spider-leg")) do
 end
 
 for _, prototype in pairs(data.raw["spider-leg"]) do
-  prototype.collision_mask = spider_leg_layers
+  prototype.collision_mask = spider_leg_alt_layers
+end
+
+
+-- The above collision mask changes make train selection priority lower than the track's
+for _, type in pairs({"artillery-wagon", "cargo-wagon", "fluid-wagon", "locomotive", "car"}) do
+  for _, prototype in pairs(data.raw[type]) do
+    if not prototype.selection_priority or prototype.selection_priority == 50 then
+      prototype.selection_priority = 51
+    end
+  end
+end
+
+-- Now that vehicles have selection_priority = 51, bump up all spidertrons to 52
+for _, type in pairs({"spider-vehicle"}) do
+  for _, prototype in pairs(data.raw[type]) do
+    if prototype.selection_priority and prototype.selection_priority == 51 then
+      prototype.selection_priority = 52
+    end
+  end
 end
